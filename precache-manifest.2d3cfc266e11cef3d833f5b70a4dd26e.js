2256b2a1d4cb9243128f68eac68ca6c3c8656b8b
Aaron <aaroner@yahoo.com>
2019-07-26 16:24:36 -0500

    Updates

---
+++ precache-manifest.2d3cfc266e11cef3d833f5b70a4dd26e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97cac220dbd0eb654649c4fe0def5fc2",
+    "url": "/reduxtest/index.html"
+  },
+  {
+    "revision": "8bcffa284694b7cbbc59",
+    "url": "/reduxtest/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/reduxtest/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "8bcffa284694b7cbbc59",
+    "url": "/reduxtest/static/js/main.fa8a1574.chunk.js"
+  },
+  {
+    "revision": "44830badc1ab1f59177d",
+    "url": "/reduxtest/static/js/runtime~main.f7f48115.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reduxtest/static/media/logo.5d5d9eef.svg"
+  }
+]);

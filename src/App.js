a57599500f5f116561036eed0c8db3bc9768408d
Aaron <aaroner@yahoo.com>
2019-07-31 17:08:06 -0500

    5/21

--- src/App.js
+++ src/App.js
@@ -28,6 +28,41 @@ function App() {
 
 export default App;
 
-const hello = () => ({ welcome: 'hello' });
-const store = createStore(hello);
+const defaultState = {
+  welcome: 'Hi',
+  otherState: 'some stuff',
+  otherStates: 'some other stuff',
+};
+
+// First reducer(a function, takes previous state, merges changes, outputs new state object) :3
+const greeting = (state = defaultState, action) => {
+  switch (action.type) {
+    case 'GREET_NAME':
+      return { ...state, welcome: `Hello ${action.name}` };
+    case 'GREET_WORLD':
+      return { ...state, welcome: 'Hello World' };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(greeting);
+
+console.log(store.getState());
+
+const name = 'Aaron';
+
+// Action - simply just an object, must dispatch for it to do anything
+store.dispatch({
+  type: 'GREET_NAME',
+  name,
+});
+
+console.log(store.getState());
+
+store.dispatch({
+  type: 'GREET_NAME',
+  name: 'Nora',
+});
+
 console.log(store.getState());
